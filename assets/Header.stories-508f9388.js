97cb6b0c42b36ed6ad8b5d408316db7d0d738e2a
tannineo <tannineo@outlook.com>
2023-01-24 13:28:02 +0000

    Updates

--- assets/Header.stories-508f9388.js
+++
@@ -1,8 +0,0 @@
-import{H as t}from"./Header-088e71c3.js";import"./Button-eb60318c.js";import"./index-248772ff.js";import"./_commonjsHelpers-042e6b4d.js";const u={title:"Example/Header",component:t,tags:["autodocs"],parameters:{layout:"fullscreen"}},e={args:{user:{name:"Jane Doe"}}},r={};var o;e.parameters={...e.parameters,storySource:{source:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...(o=e.parameters)==null?void 0:o.storySource}};var a;r.parameters={...r.parameters,storySource:{source:"{}",...(a=r.parameters)==null?void 0:a.storySource}};const c=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,c as __namedExportsOrder,u as default};
-//# sourceMappingURL=Header.stories-508f9388.js.map
---
+++ assets/preview-95782ab7.js
@@ -0,0 +1,2 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},darkMode:{current:"dark"}};export{o as parameters};
+//# sourceMappingURL=preview-95782ab7.js.map
